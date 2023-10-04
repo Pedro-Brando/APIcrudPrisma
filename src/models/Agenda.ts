8748ba6d06f88f302851762e836dc21c16db4650
Pedro Brando <62448483+Pedro-Brando@users.noreply.github.com>
2023-10-04 09:28:31 -0300

    Delete src directory

--- src/models/Agenda.ts
+++
@@ -1,9 +0,0 @@
-import { Prisma } from '@prisma/client';
-
-export const Paciente: Prisma.Model<Agenda> = {
-  name: 'Consulta',
-  properties: {
-    data: 'date',
-    nomePcnt: 'string',
-  },
-};
--- src/models/Secretaria.ts
+++
@@ -1,9 +0,0 @@
-import { Prisma } from '@prisma/client';
-
-export const Paciente: Prisma.Model<Secretaria> = {
-  name: 'Secretaria',
-  properties: {
-    nome: 'string',
-    RG: 'int',
-  },
-};
